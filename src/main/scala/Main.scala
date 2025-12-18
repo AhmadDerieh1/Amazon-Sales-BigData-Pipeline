eb9d86bfbf5b30b8d805917235e13797c2aafb5c
Ahmad Derieh <AhmadDerieh1@gmail.com>
2025-12-18 16:38:18 +0200

    Refactor Spark streaming pipeline and improve data cleaning

--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().master("local[*]").getOrCreate()
-    println("OK")
-    spark.stop()
-  }
-}
