fe1c26abf8d4195d0e65040be1fd0176de371d03
Ahmad Derieh <AhmadDerieh1@gmail.com>
2025-12-19 21:27:11 +0200

    Add MongoDB sink with probabilistic analytics

--- src/main/scala/pipeline/MainApp.scala
+++ src/main/scala/pipeline/MainApp.scala
@@ -6,15 +6,21 @@ object MainApp {
     val spark = SparkConfig.createSpark("Kafka Spark Streaming Clean")
     spark.sparkContext.setLogLevel("WARN")
 
-    val kafkaDF   = KafkaSource.readKafka(spark, "localhost:9092", "sales-topic")
-    val rawValue  = Parser.extractJsonValue(kafkaDF)
-    val parsed    = Parser.parse(rawValue)
-    val cleaned   = Cleaner.clean(parsed)
+    val kafkaDF  = KafkaSource.readKafka(spark, "localhost:9092", "sales-topic")
+    val rawValue = Parser.extractJsonValue(kafkaDF)
+    val parsed   = Parser.parse(rawValue)
+    val cleaned  = Cleaner.clean(parsed)
 
-    val query = ConsoleSink.writeToConsole(
-      cleaned,
-      "C:/Users/ahmad/spark-checkpoints/sales-topic-clean-run1"
+    val query = MongoSink.writeToMongoWithProbabilistic(
+      df              = cleaned,
+      checkpoint      = s"C:/Users/ahmad/spark-checkpoints/mongo_${System.currentTimeMillis()}",
+      mongoUri        = "mongodb://localhost:27017",
+      database        = "amazon_sales",
+      collectionClean = "sales_clean",
+      collectionStats = "sales_stats",
+      collectionNewProducts = "sales_new_products"
     )
+
     query.awaitTermination()
   }
 }
